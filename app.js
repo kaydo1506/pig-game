6c3914e70304bfecbc0de6a4e090de9912fe4dad
kaydo1506 <okedoifeoma@yahoo.com>
2020-11-02 21:32:31 +0100

    changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+document.getElementById('current-1p').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     // 1. Random number
